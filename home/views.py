80b24ec3c08b59ad3e3938d24a17e9fe9e8ca66f
azamjon180 <azamjonaliyev180@gmail.com>
2020-09-11 17:53:02 +0500

    first commit

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	template_name = 'home.html'
+	context = {}
+	return render(request, template_name, context)
+
+
+def about(request):
+	template_name = 'about.html'
+	context = {}
+	return render(request, template_name, context)
+
+
+def service(request):
+	template_name = 'service.html'
+	context = {}
+	return render(request, template_name, context)
